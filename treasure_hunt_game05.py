986864ada78dcdb68d9784e081f6aa1dc6058dcd
Tuleshwar Tiwari <ttuleshwar7@gmail.com>
2025-06-06 00:21:21 +0530

    first commit

---
+++ treasure_hunt_game05.py
@@ -0,0 +1,38 @@
+print("""  ,d                                                                       
+  88                                                                       
+MM88MMM 8b,dPPYba,  ,adPPYba, ,adPPYYba, ,adPPYba, 88       88 8b,dPPYba,  
+  88    88P'   "Y8 a8P_____88 ""     `Y8 I8[    "" 88       88 88P'   "Y8  
+  88    88         8PP""""""" ,adPPPPP88  `"Y8ba,  88       88 88          
+  88,   88         "8b,   ,aa 88,    ,88 aa    ]8I "8a,   ,a88 88          
+  "Y888 88          `"Ybbd8"' `"8bbdP"Y8 `"YbbdP"'  `"YbbdP'Y8 88          
+                                                                           
+                                                                           
+                                                               
+           88           88                                 88  
+           ""           88                                 88  
+                        88                                 88  
+ ,adPPYba, 88 ,adPPYba, 88 ,adPPYYba, 8b,dPPYba,   ,adPPYb,88  
+a8P_____88 88 I8[    "" 88 ""     `Y8 88P'   `"8a a8"    `Y88  
+8PP""""""" 88  `"Y8ba,  88 ,adPPPPP88 88       88 8b       88  
+"8b,   ,aa 88 aa    ]8I 88 88,    ,88 88       88 "8a,   ,d88  
+ `"Ybbd8"' 88 `"YbbdP"' 88 `"8bbdP"Y8 88       88  `"8bbdP"Y8  \n\n""")
+
+print("Welcome to the treasure island. Your mission is to find the treasure.")
+
+t1 = input('You\'r on a cross road would you like to go "left" or "right""?\n').lower()
+
+if t1 == "left":
+
+    t2 = input("Nou you are on a bank of river would you like to swim or wait?\n").lower()
+
+    if t2 == "wait":
+
+        d1 = input("Ohh Magic door appears would you like to choose red, blue or yellow?\n").lower()
+        if d1 == "yellow":
+            print("You Won!!! Congratulations!")
+        else:
+            print("You Lost!!! Game Over")
+    else:
+        print("You lost!!! Game Over")
+else:
+    print("You lost!!! Game Over")
