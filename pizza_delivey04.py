953c93e06340c2ef8b2f9b531432df8d1253d396
Tuleshwar Tiwari <ttuleshwar7@gmail.com>
2025-06-06 00:42:16 +0530

    Another application

--- pizza_delivey04.py
+++ pizza_delivey04.py
@@ -1,32 +1,26 @@
 print("Welcome to python pizza delivery:")
 rate=0
-size=input("Please enter the size of the pizza: s for small, m for medium and l for large:")
-pep=input("Would you like peporani: y for yes and n for no")
-extra_cheese=input("Would you like extra cheese: y for yes and n for no")
+size=input("Please enter the size of the pizza: s for small, m for medium and l for large: \n")
+
+
 if size == "s":
     rate = 15
-    if pep == "y":
-        rate += 2
-    if extra_cheese == "y":
-        rate += 1
-
 elif size == "m":
     rate = 20
-    if pep == "y":
-        rate += 3
-    if extra_cheese == "y":
-        rate += 1
-
-
-
 elif size == "l":
     rate = 25
-    if pep == "y":
-        rate += 3
-    if extra_cheese == "y":
-        rate += 1
-
 else:
     print("you typed invalid input:")
 
+pep=input("Would you like peporani: y for yes and n for no: \n")
+extra_cheese=input("Would you like extra cheese: y for yes and n for no: \n")
+
+if pep == "y":
+    if size == 's':
+        rate += 2
+    else:
+        rate +=3
+
+if extra_cheese == "y":
+    rate += 1
 print(f"the rate of pizza is {rate}")
